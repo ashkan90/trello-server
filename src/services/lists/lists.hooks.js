e97d45183006867febf98f405420729df31cdb9e
Emirhan <emirhan.1ataman@gmail.com>
2019-02-13 23:39:18 +0300

    restircted mode used

--- src/services/lists/lists.hooks.js
+++ src/services/lists/lists.hooks.js
@@ -1,9 +1,10 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
+const { isBoardOwner } = require('../../hooks/authorization');
 
 module.exports = {
   before: {
-    all: [ authenticate('jwt') ],
-    find: [],
+    all: [ authenticate('jwt'), isBoardOwner ],
+    find: [  ],
     get: [],
     create: [],
     update: [],
